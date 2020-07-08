f795b99713119917ca605cc36f109f80cb4b5144
William Byrd <webyrd@gmail.com>
2020-07-07 23:58:18 -0400

    working on outline

--- primer.scm
+++ primer.scm
@@ -1,2 +1,31 @@
 ;; Scheme Primer
 
+;; cons
+
+;; car, cdr
+
+;; quote
+
+;; box and pointers
+
+;; s-expressions
+
+;; conditionals
+
+;; definitions
+
+;; variables, scope, binding, shadowing
+
+;; lambda and application
+
+;; what does this expression evaluate to?
+
+;; recursion
+
+;; quasiquote and unquote
+
+;; pattern-matching
+
+;; interpreters
+
+;; macros
