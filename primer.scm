a9415592f2cfef59e4cecdd5a303ef639dd9c890
William Byrd <webyrd@gmail.com>
2020-07-08 00:43:37 -0400

    working on intro

--- primer.scm
+++ primer.scm
@@ -1,8 +1,11 @@
 ;; Scheme Primer
 
+(define primer-version-string "0.1")
+
 (define print-greeting
   (lambda ()
-    (display "Welcome to the Scheme Primer.")
+    (display "Welcome to the Scheme Primer, version ")
+    (display primer-version-string)
     (newline)
     (newline)
     (display "Scheme is a programming language, and is a dialect of the LISP family of languages.")
@@ -12,6 +15,9 @@
     (newline)
     (newline)
     (display "The Scheme Primer will automatically adjust the difficulty of the exercises depending on how well you have performed on previous exercises.")
+    (newline)
+    (newline)
+    (display "The Scheme Primer is itself written in Scheme.  The Scheme Primer will teach you how to implement enough Scheme to run the Primer in your own Scheme implementation (\"Primer-ception\").")
     (newline)
     (newline)
     (display "The Scheme Primer also allows you to jump to specific lessons or pratice exercises.")
