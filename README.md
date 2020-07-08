5fa869071e4cc734fcee32c575989596887a1935
William Byrd <webyrd@gmail.com>
2020-07-08 00:18:03 -0400

    working on outline

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scheme-primer
-Inspired by 'The Diamond Age', spaced repetition, and my experience teaching Scheme over many years.
+Inspired by 'The Diamond Age', spaced repetition, Rocksmith, 'The Little Schemer', Dan Friedman's C311 course, and my experience teaching Scheme over many years.
+
--- primer.scm
+++ primer.scm
@@ -1,5 +1,15 @@
 ;; Scheme Primer
 
+(printf "Welcome to the Scheme Primer.\n\n")
+
+(printf "Scheme is a programming language, and is a dialect of the LISP family of languages.\n\n")
+
+(printf "This interactive Scheme Primer will teach you Scheme, and challenge you to write Scheme code of increasing complexity.\n\n")
+
+(printf "The Scheme Primer will automatically adjust the difficulty of the exercises depending on how well you have performed on previous exercises.\n\n")
+
+(printf "The Scheme Primers also allows you to jump to specific lessons or pratice exercises.")
+
 ;; self-evaluating literals
 
 ;; cons
