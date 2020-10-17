989ebf6a84cbe6d5700fdf6179a2c4e30f62742d
William Byrd <webyrd@gmail.com>
2020-10-17 11:44:18 -0400

    working on cons

--- cons/cons.scm
+++ cons/cons.scm
@@ -1,7 +1,7 @@
 (load "../faster-miniKanren/mk-vicare.scm")
 (load "../faster-miniKanren/mk.scm")
 
-(define evalo/cons
+(define evalo/proper-or-improper-list
   (lambda (expr val)
     (conde
       ((== `(quote ,val) expr)
@@ -11,8 +11,31 @@
       ((fresh (e1 e2 v1 v2)
          (== `(cons ,e1 ,e2) expr)
          (== `(,v1 . ,v2) val)
-         (evalo/cons e1 v1)
-         (evalo/cons e2 v2))))))
+         (evalo/proper-or-improper-list e1 v1)
+         (evalo/proper-or-improper-list e2 v2))))))
+
+(define evalo/proper-list
+  (lambda (expr val)
+    (conde
+      ((== `(quote ,val) expr)
+       (== '() val))
+      ((fresh (e1 e2 v1 v2)
+         (== `(cons ,e1 ,e2) expr)
+         (== `(,v1 . ,v2) val)
+         (evalo/proper-or-improper-list e1 v1)
+         (evalo/proper-list e2 v2))))))
+
+(define evalo/flat-proper-list
+  (lambda (expr val)
+    (conde
+      ((== `(quote ,val) expr)
+       (== '() val))
+      ((fresh (e1 e2 v1 v2)
+         (== `(cons ,e1 ,e2) expr)
+         (== `(,v1 . ,v2) val)
+         (== `(quote ,v1) e1)
+         (animal-symbolo v1)
+         (evalo/flat-proper-list e2 v2))))))
 
 (define animal-symbolo
   (lambda (sym)
@@ -20,7 +43,8 @@
              '(cat
                dog
                fox
-               pig))))
+               pig
+               rat))))
 
 (define membero
   (lambda (x ls)
