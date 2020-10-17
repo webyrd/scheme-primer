cc9c7fa6e4a67c3e4b8f0838cbf21cb92fcca286
William Byrd <webyrd@gmail.com>
2020-10-17 11:57:32 -0400

    working on cons

--- cons/cons.scm
+++ cons/cons.scm
@@ -37,14 +37,30 @@
          (animal-symbolo v1)
          (evalo/flat-proper-list e2 v2))))))
 
+(define evalo/flat-proper-list-distinct-animals
+  (lambda (expr animals val)
+    (conde
+      ((== `(quote ,val) expr)
+       (== '() val))
+      ((fresh (e1 e2 v1 v2 animals^)
+         (== `(cons ,e1 ,e2) expr)
+         (== `(,v1 . ,v2) val)
+         (== `(quote ,v1) e1)
+         (remove-exactly-oneo v1 animals animals^)
+         (evalo/flat-proper-list-distinct-animals e2 animals^ v2))))))
+
+(define animals
+  '(cat
+    dog
+    fox
+    pig
+    rat
+    ant
+    bat))
+
 (define animal-symbolo
   (lambda (sym)
-    (membero sym
-             '(cat
-               dog
-               fox
-               pig
-               rat))))
+    (membero sym animals)))
 
 (define membero
   (lambda (x ls)
@@ -54,3 +70,25 @@
         ((== y x))
         ((=/= y x)
          (membero x rest))))))
+
+(define remove-exactly-oneo
+  (lambda (x ls ls-x)
+    (fresh (y rest)
+      (== `(,y . ,rest) ls)
+      (conde
+        ((== y x)
+         (== rest ls-x))
+        ((=/= y x)
+         (remove-exactly-oneo x rest ls-x))))))
+
+(define removeo
+  (lambda (x ls ls-x)
+    (conde
+      ((== '() ls) (== '() ls-x))
+      ((fresh (y rest)
+         (== `(,y . ,rest) ls)
+         (conde
+           ((== y x)
+            (== rest ls-x))
+           ((=/= y x)
+            (removeo x rest ls-x))))))))
