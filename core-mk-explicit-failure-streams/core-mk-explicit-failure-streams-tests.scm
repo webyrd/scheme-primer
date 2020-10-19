9882a4b9ec7c422953c7393b43c1226754d851c0
William Byrd <webyrd@gmail.com>
2020-10-19 05:49:23 -0400

    working on explicit streams version

---
+++ core-mk-explicit-failure-streams/core-mk-explicit-failure-streams-tests.scm
@@ -0,0 +1,547 @@
+(load "core-mk-explicit-failure-streams.scm")
+(load "../faster-miniKanren/test-check.scm")
+
+(test "mko-0"
+  (run* (q)
+    (mko '(run* (x)
+            (fresh (y)
+              (== (cons y y) x)))
+         q))
+  '((((var (s z)) . (var (s z))))))
+
+(test "mko-1"
+  (run* (q) (mko '(run* (x)
+                    (== x 'cat))
+                 q))
+  '((cat)))
+
+;; run 2 diverges (didn't diverge when we didn't support explicit failure)
+;;
+;; can we do better?
+(test "mko-1b"
+  (run 1 (e)
+    (mko `(run 1 (x)
+            (== ',e x))
+         '(cat)))
+  '(cat))
+
+;; `run 2` appears to diverge, which is expected, since there are
+;; infinitely many expressions `e`, and only 'cat will satisfy the
+;; `==` constraint.
+(test "mko-1c"
+  (run 1 (e)
+    (mko `(run* (x)
+            (== ,e x))
+         '(cat)))
+  '('cat))
+
+(test "mko occur check violation-1"
+  (run* (q)
+    (mko `(run* (x)
+            (== (cons x x) x))
+         q))
+  '(()))
+
+(test "mko occur check violation-2"
+  (run* (q)
+    (mko `(run* (x)
+            (== (cons x x) x))
+         '()))
+  '(_.0))
+
+(test "mko fail backwards-0"
+  (run 1 (e)
+    (mko `(run* (x)
+            (== (cons ,e ,e) x))
+         '()))
+  '(x))
+
+(test "mko fail backwards-1"
+  (run 8 (e)
+    (mko `(run* (x)
+            (== ,e x))
+         '()))
+  '(((cons '_.0 x) (=/= ((_.0 var))) (sym _.0))
+    (cons '() x)
+    ((cons '(_.0 . _.1) x)
+     (=/= ((_.0 var)) ((_.1 var)))
+     (sym _.0 _.1))
+    ((cons '(_.0) x)
+     (=/= ((_.0 var))) (sym _.0))
+    ((cons '(() . _.0) x)
+     (=/= ((_.0 var))) (sym _.0))
+    (cons '(()) x)
+    ((cons x '_.0) ;; a nice theorem!
+     (absento (var _.0)))
+    ((cons '(_.0 _.1 . _.2) x)
+     (=/= ((_.0 var)) ((_.1 var)) ((_.2 var)))
+     (sym _.0 _.1 _.2))))
+
+(test "mko fail backwards-2"
+  (run 8 (e)
+    (mko `(run* (q) ,e) '()))
+  '(((== '_.0 '_.1)
+     (=/= ((_.0 _.1)) ((_.0 var)) ((_.1 var)))
+     (sym _.0 _.1))
+    ((== '_.0 '())
+     (=/= ((_.0 var)))
+     (sym _.0))
+    ((== '() '_.0)
+     (=/= ((_.0 var)))
+     (sym _.0))
+    ((fresh (_.0) (== '_.1 '_.2))
+     (=/= ((_.1 _.2)) ((_.1 var)) ((_.2 var)))
+     (sym _.0 _.1 _.2))
+    ((fresh (_.0) (== '_.1 '()))
+     (=/= ((_.1 var)))
+     (sym _.0 _.1))
+    ((== '_.0 '(_.1 . _.2))
+     (=/= ((_.0 var)))
+     (sym _.0)
+     (absento (var _.1) (var _.2)))
+    ((fresh (_.0) (== '() '_.1))
+     (=/= ((_.1 var)))
+     (sym _.0 _.1))
+    ((== '(_.0 . _.1) '_.2)
+     (=/= ((_.2 var)))
+     (sym _.2)
+     (absento (var _.0) (var _.1)))))
+
+
+(test "mko-2"
+  (run* (q) (mko '(run* (x)
+                    (conde
+                      ((== x 'cat))
+                      ((== 'dog x))))
+                 q))
+  '((cat) (dog)))
+
+(test "mko-3"
+  (run* (q) (mko '(run* (x)
+                    (== 'cat 'cat))
+                 q))
+  '(((var z))))
+
+(test "mko-4"
+  (run* (q) (mko '(run* (x)
+                    (fresh (y)
+                      (== 'cat 'cat)))
+                 q))
+  '(((var z))))
+
+(test "mko-5"
+  (run* (q) (mko '(run* (x)
+                    (fresh (y)
+                      (== x 'cat)))
+                 q))
+  '((cat)))
+
+(test "mko-6"
+  (run* (q) (mko '(run* (x)
+                    (fresh (x)
+                      (== x 'cat)))
+                 q))
+  '(((var z))))
+
+(test "mko-7"
+  (run* (q) (mko '(run* (x)
+                    (fresh (y)
+                      (== y 'cat)))
+                 q))
+  '(((var z))))
+
+(test "mko-8"
+  (run* (q) (mko '(run* (x)
+                    (fresh (y)
+                      (== y 'cat)
+                      (== x y)))
+                 q))
+  '((cat)))
+
+(test "mko-9"
+  (run* (q) (mko '(run* (x)
+                    (fresh (y)
+                      (== x y)
+                      (== y 'cat)))
+                 q))
+  '((cat)))
+
+(test "mko-10"
+  (run* (q) (mko '(run* (x)
+                    (fresh (y)
+                      (fresh (z)
+                        (== (cons y z) x)
+                        (== z 'cat))
+                      (== y 'dog)))
+                 q))
+  '(((dog . cat))))
+
+(test "mko-11a"
+  (run* (q) (mko '(run* (x)
+                    (== '() x))
+                 q))
+  '((())))
+
+(test "mko-11"
+  (run* (q) (mko '(run* (x)
+                    (fresh (y)
+                      (fresh (z)
+                        (== (cons y (cons z '())) x)
+                        (== z 'cat))
+                      (== y 'dog)))
+                 q))
+  '(((dog cat))))
+
+(test "mko-12"
+  (run* (q) (mko '(run* (x) (== '(cat) x)) q))
+  '(((cat))))
+
+(test "mko-13"
+  (run* (q) (mko '(run* (x) (== '(cat dog fish) x)) q))
+  '(((cat dog fish))))
+
+(test "mko-14"
+  (run* (q) (mko '(run* (x) (== '(cat dog . fish) x)) q))
+  '(((cat dog . fish))))
+
+(test "mko-15"
+  (run* (q) (mko '(run* (x) (== '(cat dog . fish) (cons x '(dog . fish)))) q))
+  '((cat)))
+
+(test "mko-16"
+  (run* (q) (mko '(run* (x) (== (cons x '(dog . fish)) '(cat dog . fish))) q))
+  '((cat)))
+
+
+(test "mko unify x with itself"
+  (run* (q) (mko '(run* (x) (== x x)) q))
+  '(((var z))))
+
+(test "mko unify x with (cons y y)"
+  (run* (q)
+    (mko '(run* (x)
+            (fresh (y)
+              (== (cons y y) x)))
+         q))
+  '((((var (s z)) var (s z)))))
+
+(test "mko occur-check-1"
+  (run* (q) (mko '(run* (x) (== (cons x x) x)) q))
+  '(()))
+
+(test "mko occur-check-2"
+  (run* (q) (mko '(run* (x) (== x (cons x x))) q))
+  '(()))
+
+(test "mko occur-check-3"
+  (run* (q)
+    (mko '(run* (x)
+            (fresh (y)
+              (== (cons y y) x)
+              (== x y)))
+         q))
+  '(()))
+
+(test "mko occur-check-4"
+  (run* (q)
+    (mko '(run* (x)
+            (fresh (y)
+              (== x y)
+              (== (cons y y) x)))
+         q))
+  '(()))
+
+(test "mko backwards-1"
+  (run 10 (e) (mko e '(cat)))
+  '(((run 1 (_.0) (== 'cat _.0))
+     (sym _.0)) ((run 1 (_.0) (== _.0 'cat)) (sym _.0))
+    ((run 1 (_.0)
+       (conde
+         ((== 'cat _.0))
+         (_.1)))
+     (sym _.0))
+    ((run 1 (_.0)
+       (conde
+         (_.1)
+         ((== 'cat _.0))))
+     (sym _.0))
+    ((run 1 (_.0)
+       (conde
+         ((== _.0 'cat))
+         (_.1)))
+     (sym _.0))
+    ((run 1 (_.0)
+       (conde
+         (_.1)
+         ((== _.0 'cat))))
+     (sym _.0))
+    ((run 1 (_.0)
+       (fresh (_.1)
+         (== '_.2 '_.2)
+         (fresh (_.3)
+           (== 'cat _.3))))
+     (=/= ((_.2 var)))
+     (sym _.0 _.1 _.2 _.3))
+    ((run 1 (_.0)
+       (fresh (_.1)
+         (fresh (_.2) (== '_.3 '_.3))
+         (fresh (_.4) (== 'cat _.4))))
+     (=/= ((_.3 var)))
+     (sym _.0 _.1 _.2 _.3 _.4))
+    ((run 1 (_.0)
+       (fresh (_.1)
+         (== '_.2 '_.2)
+         (fresh (_.3) (== _.3 'cat))))
+     (=/= ((_.2 var)))
+     (sym _.0 _.1 _.2 _.3))
+    ((run 1 (_.0)
+       (fresh (_.1) (== 'cat _.0)))
+     (=/= ((_.0 _.1)))
+     (sym _.0 _.1))))
+
+(test "mko backwards-2"
+  (run 1 (e)
+    (mko e '(cat))
+    (mko e '(dog)))
+  '(((run 1 (_.0)
+       (conde
+         ((== 'cat _.0))
+         ((== 'dog _.0))))
+     (sym _.0))))
+
+
+(test "eval-mko 0"
+  (run 1 (subst^)
+    (fresh (c)
+      (eval-mko '(== 'cat 'cat) '() 'z c '() subst^)))
+  '(()))
+
+(test "eval-mko 1"
+  (run 1 (expr subst^)
+    (fresh (c)
+      (eval-mko expr '() 'z c '() subst^)))
+  '((((== (quote _.0) (quote _.1)) #f)
+     (=/= ((_.0 _.1)) ((_.0 var)) ((_.1 var)))
+     (sym _.0 _.1))))
+
+(test "eval-mko 2"
+  (run* (subst^)
+    (fresh (c)
+      (eval-mko '(== 'cat 'cat) '() 'z c '() subst^)))
+  '(()))
+
+(test "eval-mko 3"
+  (run* (subst^)
+    (fresh (c)
+      (eval-mko '(== 'cat 'dog) '() 'z c '() subst^)))
+  '(#f))
+
+(test "eval-mko 4"
+  (run* (subst^)
+    (fresh (c)
+      (eval-mko '(fresh (x)
+                   (== x 'cat)
+                   (== 'dog 'dog))
+                '() 'z c '() subst^)))
+  '((((var z) . cat))))
+
+(test "eval-mko 5"
+  (run* (subst^)
+    (fresh (c)
+      (eval-mko '(fresh (x)
+                   (== x 'cat)
+                   (== 'cat x))
+                '() 'z c '() subst^)))
+  '((((var z) . cat))))
+
+(test "eval-mko 6"
+  (run* (subst^)
+    (fresh (c)
+      (eval-mko '(fresh (x)
+                   (== x 'cat) (== 'dog x))
+                '() 'z c '() subst^)))
+  '(#f))
+
+(test "eval-mko 7"
+  (run* (subst^)
+    (fresh (c)
+      (eval-mko '(fresh (x)
+                   (conde
+                     ((== x 'cat))
+                     ((== x 'dog))))
+                '() 'z c '() subst^)))
+  '((((var z) . cat)) (((var z) . dog))))
+
+(test "eval-mko 8"
+  (run* (subst^)
+    (fresh (c)
+      (eval-mko '(fresh (x)
+                   (conde
+                     ((== 'cat 'cat))
+                     ((== x 'dog))))
+                '() 'z c '() subst^)))
+  '(() (((var z) . dog))))
+
+(test "eval-mko 9"
+  (run* (subst^)
+    (fresh (c)
+      (eval-mko '(conde
+                   ((== 'cat 'cat))
+                   ((== 'dog 'dog)))
+                '() 'z c '() subst^)))
+  '(() ()))
+
+(test "eval-mko 10"
+  (run* (subst^)
+    (fresh (c)
+      (eval-mko '(fresh (x)
+                   (conde
+                     ((fresh (y)
+                        (== y 'cat)
+                        (== 'fish y)))
+                     ((== x 'dog))))
+                '() 'z c '() subst^)))
+  '((((var z) . dog)) #f))
+
+(test "eval-mko 11"
+  (run* (subst^)
+    (fresh (c)
+      (eval-mko '(fresh (x)
+                   (conde
+                     ((== x 'dog))
+                     ((fresh (y)
+                        (== 'fish y)
+                        (== y 'cat)))))
+                '() 'z c '() subst^)))
+  '((((var z) . dog)) #f))
+
+(test "eval-mko backwards-1"
+  (run 2 (expr)
+    (fresh (c)
+      (eval-mko expr '() 'z c '() '(((var z) . dog)))))
+  '(((fresh (_.0) (== 'dog _.0))
+     (sym _.0))
+    ((fresh (_.0) (== _.0 'dog))
+     (sym _.0))))
+
+(test "eval-mko backwards-2"
+  (run 10 (expr)
+    (fresh (c)
+      (eval-mko expr '() 'z c '() '(((var z) . dog)))))
+  '(((fresh (_.0) (== 'dog _.0))
+     (sym _.0))
+    ((fresh (_.0) (== _.0 'dog))
+     (sym _.0))
+    ((fresh (_.0)
+       (== '_.1 '_.1)
+       (== 'dog _.0))
+     (=/= ((_.1 var)))
+     (sym _.0 _.1))
+    ((fresh (_.0)
+       (== '_.1 '_.1)
+       (== _.0 'dog))
+     (=/= ((_.1 var)))
+     (sym _.0 _.1))
+    ((fresh (_.0)
+       (fresh (_.1)
+         (== '_.2 '_.2))
+       (== 'dog _.0))
+     (=/= ((_.2 var)))
+     (sym _.0 _.1 _.2))
+    ((fresh (_.0)
+       (fresh (_.1)
+         (== 'dog _.0)))
+     (=/= ((_.0 _.1)))
+     (sym _.0 _.1))
+    ((fresh (_.0)
+       (conde
+         ((== 'dog _.0))
+         (_.1)))
+     (sym _.0))
+    ((conde
+       ((fresh (_.0)
+          (== 'dog _.0)))
+       (_.1))
+     (sym _.0))
+    ((conde
+       (_.0)
+       ((fresh (_.1) (== 'dog _.1))))
+     (sym _.1))
+    ((fresh (_.0)
+       (conde
+         (_.1)
+         ((== 'dog _.0))))
+     (sym _.0))))
+
+
+(test "walko the dog"
+  (run* (q) (walko 'dog '(((var z) . cat)) q))
+  '(dog))
+
+(test "walko the dogs"
+  (run* (q) (walko '(dog . dog) '(((var z) . cat)) q))
+  '((dog . dog)))
+
+(test "walko the vars"
+  (run* (q) (walko '((var z) . (var z)) '(((var z) . cat)) q))
+  '(((var z) var z)))
+
+(test "walko 1"
+  (run* (q) (walko '(var z) '(((var z) . cat)) q))
+  '(cat))
+
+(test "walko 2"
+  (run* (q) (walko '(var (s (s (s z)))) '(((var (s (s (s z)))) . cat)) q))
+  '(cat))
+
+(test "walko 3"
+  (run* (q) (walko '(var (s (s (s z)))) '(((var (s (s z))) . cat)) q))
+  '((var (s (s (s z))))))
+
+
+
+(test "walk*o the vars"
+  (run* (q) (walk*o '((var z) . (var z)) '(((var z) . cat)) q))
+  '((cat . cat)))
+
+
+
+(test "unifyo 1"
+  (run* (subst^)
+    (unifyo 'cat 'cat '() subst^))
+  '(()))
+
+(test "unifyo 2"
+  (run* (subst^)
+    (unifyo 'cat 'dog '() subst^))
+  '(#f))
+
+(test "unifyo 3"
+  (run* (subst^)
+    (unifyo '(var z) '((var (s z))) '() subst^))
+  '((((var z) (var (s z))))))
+
+(test "unifyo 4"
+  (run* (subst^)
+    (unifyo '(var z) '((var z)) '() subst^))
+  '(#f))
+
+(test "unifyo 5"
+  (run* (subst^)
+    (unifyo '(var z) '(var z) '() subst^))
+  '(()))
+
+(test "unifyo 6"
+  (run* (subst^)
+    (unifyo '(var z) '(var (s z)) '() subst^))
+  '((((var z) . (var (s z))))))
+
+(test "unifyo 7"
+  (run* (subst^)
+    (unifyo '(var z) '((var z)) '() subst^))
+  '(#f))
+
+(test "unifyo 8"
+  (run* (subst^)
+    (unifyo '((var z)) '(var z) '() subst^))
+  '(#f))
---
+++ core-mk-explicit-failure-streams/core-mk-explicit-failure-streams.scm
@@ -0,0 +1,324 @@
+(load "../faster-miniKanren/mk-vicare.scm")
+(load "../faster-miniKanren/mk.scm")
+
+;; Relational environment-passing, substitution-passing interpreter
+;; for a subset of miniKanren, written in miniKanren.
+;;
+;; This version of the interpreter explicitly represents failure, in
+;; addition to success, and explicitly represents streams.
+
+#|
+Grammar:
+
+;; run* expression
+run1-expr ::= (run* (<x>) <ge>)
+
+;; goal expression
+<ge> ::= (== <e> <e>) |
+         (fresh (<x>) <ge>) |
+         (fresh (<x>) <ge> <ge>) |
+         (conde (<ge>) (<ge>))
+
+;; Scheme expression
+<e> ::= <x> |
+        (quote <datum>) |
+        (cons <e> <e>)
+
+;; Scheme lexical variable
+<x> ::= <symbol>
+
+;; quoted datum
+<datum> ::= <symbol> |
+            () |
+            (<datum> . <datum>)
+|#
+
+;; Logic variables are represented as tagged lists of the form `(var ,c)`
+;; where `c` is a Peano numeral of the form `z`, `(s z)`, `(s (s z))`, etc.
+;; Logic variables that remain fresh are reified as themselves, rather than
+;; being replaced with `_.0`, `_.1`, etc.
+
+
+;; TODO:
+;;
+;; Think about reification of fresh logic variables--should it work
+;; like in regular mk, by using some kind of fake subst?  If so, would
+;; you be able to tell whether '_.0' came from miniKanren, or the
+;; language being interpreted?
+;;
+;; Support =/=, symbolo, numbero, and absento
+;;
+;; Support helpers and recursion
+
+(define mzero '())
+(define (unit s/c) (cons s/c mzero))
+
+(define (mko expr out)
+  (fresh (q ge $)
+    (== `(run* (,q) ,ge) expr)
+    (symbolo q)
+    (eval-mko
+     ;; goal expression
+     ge
+     ;; initial env
+     `((,q . (var z)))
+     ;; initial s/c
+     '(() . (s z))
+     ;; resulting stream of s/c's
+     $)
+    
+    ;; TODO -- `walk*` the query variable over the stream of s/c's
+    ;; For now we will punt, and just unify `out` with the entire stream.
+    (== $ out)
+    
+    ))
+
+(define (eval-mko expr env s/c $)
+  (conde
+    ((fresh (e1 e2 t1 t2 s s^ c)
+       (== `(== ,e1 ,e2) expr)
+       (== `(,s . ,c) s/c)
+       (evalo e1 env t1)
+       (evalo e2 env t2)
+       (conde
+         ((== #f s^) (== mzero $))
+         ((=/= #f s^) (== (unit `(,s^ . (s ,c))) $)))
+       (unifyo t1 t2 s s^)))
+    ((fresh (x ge s c)
+       (== `(fresh (,x) ,ge) expr)
+       (symbolo x)
+       (== `(,s . ,c) s/c)
+       (eval-mko ge `((,x . (var ,c)) . ,env) `(,s . (s ,c)) $)))
+    ((fresh (x ge1 ge2 s c $^)
+       (== `(fresh (,x) ,ge1 ,ge2) expr)
+       (symbolo x)
+       (== `(,s . ,c) s/c)
+       (eval-mko ge1 `((,x . (var ,c)) . ,env) `(,s . (s ,c)) $^)
+       (bindo $^ ge2 env $)))
+    ((fresh (ge1 ge2 $1 $2)
+       (== `(conde (,ge1) (,ge2)) expr)
+       (eval-mko ge1 env s/c $1)
+       (eval-mko ge2 env s/c $2)
+       (mpluso $1 $2 $)))))
+
+(define (mpluso $1 $2 $^)
+  (conde
+    ((== '() $1) (== $2 $^))
+    ;;
+    ;; no procedure/delayed clause
+    ;;
+    ;; TODO: do we need the procedure/delayed clause?  If so, how to
+    ;; implement it?  Using `evalo`?
+    ;;
+    ((fresh (a d res)
+       (== `(,a . ,d) $1)
+       (== `(,a . ,res) $^)
+       (mpluso d $2 res)))))
+
+(define (bindo $ g env $^)
+  (conde
+    ((== '() $) (== mzero $^))
+    ;;
+    ;; no procedure/delayed clause
+    ;;
+    ;; TODO: do we need the procedure/delayed clause?  If so, how to
+    ;; implement it?  Using `evalo`?
+    ;;
+    ((fresh (a d $1^ $2^)
+       (== `(,a . ,d) $)
+       ;; we need to use `eval-mko`, which takes `env`,
+       ;; to keep everything relational
+       (eval-mko g env a $1^)
+       (bindo d g env $2^)
+       (mpluso $1^ $2^ $^)))))
+
+(define (evalo expr env val)
+  (conde
+    ((== `(quote ,val) expr)
+     (absento 'var val))
+    ((symbolo expr) (lookupo expr env val))
+    ((fresh (e1 e2 v1 v2)
+       (== `(cons ,e1 ,e2) expr)
+       (== `(,v1 . ,v2) val)
+       (evalo e1 env v1)
+       (evalo e2 env v2)))))
+
+(define (lookupo x env val)
+  (fresh (y v rest)
+    (== `((,y . ,v) . ,rest) env)
+    (conde
+      ((== x y) (== v val))
+      ((=/= x y)
+       (lookupo x rest val)))))
+
+(define (unifyo t1 t2 subst subst^)
+  (fresh (t1^ t2^)
+    (walko t1 subst t1^)
+    (walko t2 subst t2^)
+    (conde
+      ;; ----- symbols -------
+      ;; symbol with symbol
+      ((symbolo t1^) (symbolo t2^)
+       (conde
+         ((== t1^ t2^) (== subst subst^))
+         ((=/= t1^ t2^) (== #f subst^))))
+      ;; symbol with empty list
+      ((symbolo t1^) (== '() t2^)
+       (== #f subst^))
+      ((symbolo t2^) (== '() t1^)
+       (== #f subst^))
+      ;; symbol with pair
+      ((fresh (a2 d2)
+         (symbolo t1^)
+         (== `(,a2 . ,d2) t2^)
+         (=/= 'var a2) ;; don't mistake tagged vars for regular pairs
+         (== #f subst^)))
+      ((fresh (a1 d1)
+         (symbolo t2^)
+         (== `(,a1 . ,d1) t1^)
+         (=/= 'var a1) ;; don't mistake tagged vars for regular pairs
+         (== #f subst^)))
+      ;; symbol with var
+      ((fresh (c1)
+         (== `(var ,c1) t1^)
+         (symbolo t2^) ;; t2^ is a literal symbol, not a var
+         (== `(((var ,c1) . ,t2^) . ,subst) subst^)))
+      ((fresh (c2)
+         (== `(var ,c2) t2^)
+         (symbolo t1^) ;; t1^ is a literal symbol, not a var
+         (== `(((var ,c2) . ,t1^) . ,subst) subst^)))
+      ;; ----- empty list -------
+      ;; empty list with empty list
+      ((== '() t1^) (== '() t2^) (== subst subst^))
+      ;; empty list with symbol -- handled above
+      ;; empty list with pair
+      ((fresh (a2 d2)
+         (== '() t1^)
+         (== `(,a2 . ,d2) t2^)
+         (=/= 'var a2) ;; don't mistake tagged vars for regular pairs
+         (== #f subst^)))
+      ((fresh (a1 d1)
+         (== '() t2^)
+         (== `(,a1 . ,d1) t1^)
+         (=/= 'var a1) ;; don't mistake tagged vars for regular pairs
+         (== #f subst^)))        
+      ;; empty list with var
+      ((fresh (c1)
+         (== `(var ,c1) t1^)
+         (== '() t2^)
+         (== `(((var ,c1) . ,t2^) . ,subst) subst^)))
+      ((fresh (c2)
+         (== `(var ,c2) t2^)
+         (== '() t1^)
+         (== `(((var ,c2) . ,t1^) . ,subst) subst^)))
+      ;; ----- var -------
+      ;; var and var
+      ((fresh (c1 c2)
+         (== `(var ,c1) t1^)
+         (== `(var ,c2) t2^)
+         (conde
+           ((== c1 c2) (== subst subst^))
+           ((=/= c1 c2) (== `(((var ,c1) . (var ,c2)) . ,subst) subst^)))))
+      ;; var with symbol -- handled above
+      ;; var with empty list -- handled above
+      ;; var with pair       
+      ((fresh (c1 a2 d2)
+         (== `(var ,c1) t1^)
+         (== `(,a2 . ,d2) t2^)
+         (=/= 'var a2) ;; don't mistake tagged vars for regular pairs
+         (conde
+           ((== `(((var ,c1) . (,a2 . ,d2)) . ,subst) subst^)
+            (not-occurso t1^ t2^))
+           ((== #f subst^)
+            (occurso t1^ t2^)))))
+      ((fresh (c2 a1 d1)
+         (== `(var ,c2) t2^)
+         (== `(,a1 . ,d1) t1^)
+         (=/= 'var a1) ;; don't mistake tagged vars for regular pairs
+         (conde
+           ((== `(((var ,c2) . (,a1 . ,d1)) . ,subst) subst^)
+            (not-occurso t2^ t1^))
+           ((== #f subst^)
+            (occurso t2^ t1^)))))
+      ;; ----- pair -------
+      ;; pair with pair
+      ((fresh (a1 d1 a2 d2 subst^^)
+         (== `(,a1 . ,d1) t1^)
+         (== `(,a2 . ,d2) t2^)
+         (=/= 'var a1) ;; don't mistake tagged vars for regular pairs
+         (=/= 'var a2) ;; don't mistake tagged vars for regular pairs
+         (unifyo a1 a2 subst subst^^)           
+         (conde
+           ((== #f subst^^)
+            (== #f subst^))
+           ((=/= #f subst^^)
+            (unifyo d1 d2 subst^^ subst^)))))
+      ;; pair with symbol -- handled above
+      ;; pair with empty list -- handled above
+      ;; pair with var -- handled above
+      )))
+
+(define (occurso x t)
+  (fresh (c)
+    (== `(var ,c) x)
+    (conde
+      ((== `(var ,c) t))
+      ((fresh (a d)
+         (== `(,a . ,d) t)
+         (=/= 'var a) ;; don't mistake tagged vars for regular pairs
+         (conde
+           ((occurso x a))
+           ((not-occurso x a)
+            (occurso x d))))))))
+
+(define (not-occurso x t)
+  (fresh (c)
+    (== `(var ,c) x)
+    (conde
+      ((symbolo t))
+      ((== '() t))
+      ((fresh (c^)
+         (== `(var ,c^) t)
+         (=/= c c^)))
+      ((fresh (a d)
+         (== `(,a . ,d) t)
+         (=/= 'var a) ;; don't mistake tagged vars for regular pairs
+         (not-occurso x a)
+         (not-occurso x d))))))
+
+(define (walko t subst t^)
+  (letrec ((walk-varo
+            (lambda (t s t^)
+              (conde
+                ((== '() s) (== t t^))
+                ((fresh (c u rest)
+                   (== `(((var ,c) . ,u) . ,rest) s)
+                   (conde
+                     ((== `(var ,c) t) (walko u subst t^))
+                     ((=/= `(var ,c) t) (walk-varo t rest t^)))))))))
+    (conde
+      ((symbolo t) (== t t^))
+      ((== '() t) (== t t^))
+      ((fresh (a d)
+         (== `(,a . ,d) t)
+         (=/= 'var a) ;; don't mistake tagged vars for regular pairs
+         (== t t^)))
+      ((fresh (c)
+         (== `(var ,c) t)
+         (walk-varo t subst t^))))))
+
+(define (walk*o t subst t^)
+  (fresh (t^^)
+    (walko t subst t^^)
+    (conde
+      ((symbolo t^^) (== t^^ t^))
+      ((== '() t^^) (== t^^ t^))
+      ((fresh (c)
+         (== `(var ,c) t^^)
+         (== t^^ t^)))
+      ((fresh (a d a^ d^)
+         (== `(,a . ,d) t^^)
+         (=/= 'var a) ;; don't mistake tagged vars for regular pairs
+         (== `(,a^ . ,d^) t^)
+         (walk*o a subst a^)
+         (walk*o d subst d^))))))
